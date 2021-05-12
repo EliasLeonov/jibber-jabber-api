8b496f1302181cca7cfa1521043afa0539295c3d
Elias <eliasleonov99@gmail.com>
2021-05-12 15:15:30 -0300

    first commit

---
+++ src/main/java/edu/austral/ingsis/JibberJabberApplication.java
@@ -0,0 +1,13 @@
+package edu.austral.ingsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberJabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberJabberApplication.class, args);
+	}
+
+}
